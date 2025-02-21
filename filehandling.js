70536e797afe74624df1f2446f0a57fa020b96e6
Ashutosh Anil Deshpande <111687968+Aesthetic017@users.noreply.github.com>
2025-02-21 22:10:49 +0000

    Add files via upload

---
+++ filehandling.js
@@ -0,0 +1,14 @@
+//create file using writeFileSync handler
+
+const fs = require("fs");
+// fs.writeFileSync('./test.txt','Hey There'); //if we change the content it will overwrite the older
+//async -
+fs.writeFile("./text.txt","hello there async",(err)=>{})
+
+//read file sync
+const result=fs.readFileSync("./contacts.txt","utf-8");
+console.log(result)
+//async
+// const res = fs.readFileSync("./contacts.txt","utf-8",(err,result)=>{
+//     console.log(res)
+// });
